9d05115410215541c375c615d0e92ac6157d11a9
torakenosu8901 <torakenosu8901@gmail.com>
2022-05-28 18:53:52 +0900

    ゲームハッカソン、二組目最初の作業。 シーン遷移を担当いたしました私「外島(としま)」と申します。 今回はスクリプトから「canvas」と「image」の作成をしまして 次の機会にImageのAlpha値をいじることで フェードインフェードアウトをしながらシーンの遷移をするように組みたいと思います。 現状では外部から特定の関数に影響を及ぼすことで作動する 受動式のコードを作成するつもりです。

--- Assets/Users/Toranosuke/Script/NextSceneTransition.cs
+++ Assets/Users/Toranosuke/Script/NextSceneTransition.cs
@@ -2,21 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class NextSceneTransition : MonoBehaviour
 {
-    private Canvas canvas;
+    private static Canvas canvas;
 
-    private Image image;
+    private static Image image;
+
+    private static NextSceneTransition instance;
+
+    [SerializeField]
+    private bool UseFadeIn = false;
+
+    [SerializeField]
+    private bool UseFadeOut = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
+        CreateCanvasAndImage();
+    }
+    private void CreateCanvasAndImage()
+    {
+        //カメラの定義をする
+        Camera targetCamera = GameObject.Find ("MainCamera").GetComponent<Camera> ();
+
+
+        //スクリプトからキャンバスを生成する処理
+        GameObject canvasObject = new GameObject("FadeCanvas");
+        canvas = canvasObject.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = targetCamera;
+        canvas.sortingOrder = 100;
+        canvasObject.AddComponent<GraphicRaycaster>();
+    
+        //スクリプトからイメージを生成する処理
+        image = new GameObject("FadeImage").AddComponent<Image>();
+        image.transform.SetParent(canvas.transform, false);
+        image.rectTransform.anchoredPosition = Vector3.zero;
+        Camera camera = Camera.main;
+        image.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+        image.color = Color.clear;
+        image.raycastTarget = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(UseFadeIn)
+        {
+
+        }
     }
 }
