bd452bf830d3b56c7c580746e815fd7f7ca9adb2
torakenosu8901 <torakenosu8901@gmail.com>
2022-06-08 04:06:35 +0900

    シーン遷移のソースコード解説
    前提 : フェードインフェードアウト用のキャンバスとイメージはスクリプトから生成するため用意する必要はなし
    
    使い方
    1.インスペクター上からstring型変数「nextscene」に遷移先のシーン名を記入
    2.フェードインを使うなら「useFadein」に、フェードアウトを使うなら「useFadeout」
    　にチェックが入っていることを確認する
    3.フェードインフェードアウトにかかる時間を「1 / fadespeed = フェードにかかる時間」のもと調整する
    4.シーンを遷移させたいタイミングで当ソースコードのpublic関数「GotoNextScene」を呼び出す
    　呼び出し方は「NextSceneTransition.instance.GotoNextScene();」と書く

--- Assets/Users/Toranosuke/Script/NextSceneTransition.cs
+++ Assets/Users/Toranosuke/Script/NextSceneTransition.cs
@@ -6,41 +6,65 @@ using UnityEngine.SceneManagement;
 
 public class NextSceneTransition : MonoBehaviour
 {
+    //キャンバスを持っておく変数
     private static Canvas canvas;
 
+    //フェード用のイメージを持っておく変数
     private static Image image;
 
-    private static NextSceneTransition instance;
+    //他スクリプトから関数を利用するためのインスタンス
+    public static NextSceneTransition instance;
 
+    //遷移先のシーン名を持っておく文字列
     [SerializeField]
-    private bool UseFadeIn = false;
+    private string nextscene = null;
 
+    //フェードインを使用するときにオンにするbool型変数
     [SerializeField]
-    private bool UseFadeOut = false;
+    private bool usefadein = true;
+
+    //フェードイアウトを使用するときにオンにするbool型変数
+    [SerializeField]
+    private bool usefadeout = true;
+
+    //関数を利用して他スクリプトからフェード遷移を実行するためのbool型public変数
+    //[SerializeField]
+    private bool startscenetransition = false;
+
+    //フェードインフェードアウトの速さ(1 / fadespeed = フェードにかかる秒数)
+    [SerializeField]
+    private float fadespeed = 0.33f;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(instance == null)
+        //instanceの有効化(?)
+        if (instance == null)
         {
             instance = this;
         }
+
         CreateCanvasAndImage();
+
+        //フェードインを使用する場合はフェード用イメージのアルファ値を最大にする
+        if(usefadein)
+        {
+            image.color = new Color(0f, 0f, 0f, 1f);
+        }
     }
+
+    /// <summary>
+    /// 初期化処理(キャンバスとフェード用のイメージを生成)
+    /// </summary>
     private void CreateCanvasAndImage()
     {
-        //カメラの定義をする
-        Camera targetCamera = GameObject.Find ("MainCamera").GetComponent<Camera> ();
-
-
         //スクリプトからキャンバスを生成する処理
         GameObject canvasObject = new GameObject("FadeCanvas");
         canvas = canvasObject.AddComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceCamera;
-        canvas.worldCamera = targetCamera;
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
         canvas.sortingOrder = 100;
         canvasObject.AddComponent<GraphicRaycaster>();
-    
+
         //スクリプトからイメージを生成する処理
         image = new GameObject("FadeImage").AddComponent<Image>();
         image.transform.SetParent(canvas.transform, false);
@@ -51,12 +75,45 @@ public class NextSceneTransition : MonoBehaviour
         image.raycastTarget = false;
     }
 
+    /// <summary>
+    /// 外部からアクセスしてUpdate内の処理を実行するための処理
+    /// </summary>
+    public void GotoNextScene()
+    {
+        startscenetransition = true;
+    }
+ 
     // Update is called once per frame
     void Update()
     {
-        if(UseFadeIn)
+        if(usefadein)   //フェードインをする処理
         {
-
+            if(image.color.a > 0f)
+            {
+                image.color -= new Color(0f, 0f, 0f, fadespeed * Time.deltaTime);
+            }
+            else
+            {
+                usefadein = false;
+            }
         }
-    }
+        else if(startscenetransition)   //フェードアウトをする処理
+        {
+            if (usefadeout)
+            {
+                if (image.color.a < 1f)
+                {
+                    image.color += new Color(0f, 0f, 0f, fadespeed * Time.deltaTime);
+                }
+                else
+                {
+                    usefadeout = false;
+                }
+            }
+            else
+            {
+                SceneManager.LoadScene(nextscene);  //シーン遷移をする処理
+            }
+        }
+    }        
 }
